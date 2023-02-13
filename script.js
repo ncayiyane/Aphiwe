5dc8633834a8d0ed1b9d97f751f20bd904dee4cb
ncayiyane <115541330+ncayiyane@users.noreply.github.com>
2023-02-13 03:55:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* ========== typing animation =========*/
+var typed = new Typed(".typing", {
+    strings:["","Web Developer","Front-end Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
